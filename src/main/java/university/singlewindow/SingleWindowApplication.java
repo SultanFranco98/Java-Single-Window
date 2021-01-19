b7c2be18d8871d4b80a8203b49fe8d80b29e2035
SultanFranco98 <s.syidaliev@gmail.com>
2021-01-20 02:30:55 +0600

    first commit

---
+++ src/main/java/university/singlewindow/SingleWindowApplication.java
@@ -0,0 +1,13 @@
+package university.singlewindow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleWindowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleWindowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/university/singlewindow/SingleWindowApplicationTests.java
@@ -0,0 +1,13 @@
+package university.singlewindow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleWindowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
