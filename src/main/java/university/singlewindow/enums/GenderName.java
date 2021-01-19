c0979c7cb18047bc3ce0a7b78dd595088b8a8d22
SultanFranco98 <s.syidaliev@gmail.com>
2021-01-20 02:42:00 +0600

    created enums

---
+++ src/main/java/university/singlewindow/enums/GenderName.java
@@ -0,0 +1,6 @@
+package university.singlewindow.enums;
+
+public enum GenderName {
+    GENDER_MALE,
+    GENDER_FEMALE
+}
---
+++ src/main/java/university/singlewindow/enums/RoleName.java
@@ -0,0 +1,16 @@
+package university.singlewindow.enums;
+
+public enum RoleName {
+    ROLE_STUDENT,
+    ROLE_DEANERY,
+    ROLE_SUPER_ADMIN,
+    ROLE_STAFF,
+    ROLE_ARCHIVE,
+    ROLE_ACCOUNTING,
+    ROLE_DORMITORY,
+    ROLE_STUDENT_OK,
+    ROLE_NAME_INTER_DEPART,
+    ROLE_LIBRARY,
+    ROLE_PASSPORT_OFFICER,
+    ROLE_DEPARTMENT
+}
